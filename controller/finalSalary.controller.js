c3da5b64b77872406c9c73933f89dd8818c7b2c5
bhawana <bhawanajupitech03@gmail.com>
2024-11-29 17:52:14 +0530

    paid

--- controller/finalSalary.controller.js
+++ controller/finalSalary.controller.js
@@ -291,6 +291,11 @@ export const salaryStatus = async (req, res, next) => {
   try {
     const { employeeId, salaryMonth } = req.params;
     const employeeSlip = await FinalSalary.findOne({ employeeId, salaryMonth });
+    if ((employeeSlip.status = "Paid")) {
+      return res
+        .status(404)
+        .json({ message: "Salary Already Paid", status: false });
+    }
     employeeSlip.status = "Paid";
     employeeSlip.save();
     res.status(200).json({ message: "Salary Paid", status: true });
